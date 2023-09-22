86851ba40f6a681a89b05936401dc970113aad71
MDGcode <adrianboscanici@yahoo.com>
2023-09-22 17:57:00 +0300

    elvis

--- client/src/views/ Concurenti.view.tsx
+++ client/src/views/ Concurenti.view.tsx
@@ -1,173 +1,163 @@
 import React, { useRef } from "react";
 import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
+import { ReactNebula } from "@flodlc/nebula";
 import Header from "./components/General/Header";
-
-// Little helpers ...
-const url = (name: string, wrap = false) =>
-  `${
-    wrap ? "url(" : ""
-  }https://awv3node-homepage.surge.sh/build/assets/${name}.svg${
-    wrap ? ")" : ""
-  }`;
-
+import Elvis from "../assets/img/elvis.jpg";
+import Beatles from "../assets/img/beatles.jpg";
+import Carousel from "./components/General/Carousel.component";
+import Jackson from "../assets/img/jackson.jpg";
+import Abba from "../assets/img/abba.jpg";
+import Nirvana from "../assets/img/nirvana.jpg";
 export default function App() {
   const parallax = useRef<IParallax>(null!);
   return (
     <>
       <Header />
-      <div style={{ width: "100%", height: "100%", background: "#253237" }}>
-        <Parallax ref={parallax} pages={3}>
-          <ParallaxLayer
-            offset={1}
-            speed={1}
-            style={{ backgroundColor: "#805E73" }}
-          />
-          <ParallaxLayer
-            offset={2}
-            speed={1}
-            style={{ backgroundColor: "#87BCDE" }}
-          />
-
+      <div>
+        <Parallax ref={parallax} pages={8}>
           <ParallaxLayer
             offset={0}
+            factor={8}
             speed={0}
-            factor={3}
             style={{
-              backgroundImage: url("stars", true),
+              backgroundImage: `url(${Elvis})`,
               backgroundSize: "cover",
             }}
-          />
-
-          <ParallaxLayer
-            offset={1.3}
-            speed={-0.3}
-            style={{ pointerEvents: "none" }}
           >
-            <img
-              src={url("satellite4")}
-              style={{ width: "15%", marginLeft: "70%" }}
-            />
+            <div className="w-full h-full object-cover object-left scale-x-[1] mx-auto">
+              <ReactNebula
+                config={{
+                  starsCount: 1000,
+                  starsColor: "#FFFFFF",
+                  starsRotationSpeed: 5,
+                  cometFrequence: 100,
+                  nebulasIntensity: 0,
+                  bgColor: "rgb(8,8,8)",
+                  sunScale: 0,
+                  planetsScale: 0,
+                  solarSystemOrbite: 70,
+                  solarSystemSpeedOrbit: 100,
+                }}
+              />
+            </div>
+            <div className="w-full h-screen absolute top-0 left-0">
+              <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                  Balul Bobocilor
+                </h1>
+              </div>
+            </div>
           </ParallaxLayer>
-
-          <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "20%", marginLeft: "55%" }}
-            />
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "10%", marginLeft: "15%" }}
-            />
-          </ParallaxLayer>
-
-          <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "20%", marginLeft: "70%" }}
-            />
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "20%", marginLeft: "40%" }}
-            />
-          </ParallaxLayer>
-
-          <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "10%", marginLeft: "10%" }}
-            />
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "20%", marginLeft: "75%" }}
-            />
-          </ParallaxLayer>
-
-          <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "20%", marginLeft: "60%" }}
-            />
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "25%", marginLeft: "30%" }}
-            />
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "10%", marginLeft: "80%" }}
-            />
-          </ParallaxLayer>
-
-          <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "20%", marginLeft: "5%" }}
-            />
-            <img
-              src={url("cloud")}
-              style={{ display: "block", width: "15%", marginLeft: "75%" }}
-            />
-          </ParallaxLayer>
-
-          <ParallaxLayer
-            offset={2.5}
-            speed={-0.4}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              pointerEvents: "none",
-            }}
-          >
-            <img src={url("earth")} style={{ width: "60%" }} />
-          </ParallaxLayer>
-
-          <ParallaxLayer
-            offset={2}
-            speed={-0.3}
-            style={{
-              backgroundSize: "80%",
-              backgroundPosition: "center",
-              backgroundImage: url("clients", true),
-            }}
-          />
-
-          <ParallaxLayer
-            offset={0}
-            speed={0.1}
-            onClick={() => parallax.current.scrollTo(1)}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <img src={url("server")} style={{ width: "20%" }} />
-          </ParallaxLayer>
-
           <ParallaxLayer
             offset={1}
-            speed={0.1}
-            onClick={() => parallax.current.scrollTo(2)}
+            factor={1}
+            speed={1}
             style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
+              backgroundImage: `url(${Elvis})`,
+              backgroundSize: "cover",
             }}
           >
-            <img src={url("bash")} style={{ width: "40%" }} />
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1.99} factor={1} speed={1}>
+            <Carousel />
           </ParallaxLayer>
 
           <ParallaxLayer
             offset={2}
-            speed={-0}
+            factor={1}
+            speed={2}
             style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
+              backgroundImage: `url(${Beatles})`,
+              backgroundSize: "cover",
             }}
-            onClick={() => parallax.current.scrollTo(0)}
           >
-            <img src={url("clients-main")} style={{ width: "40%" }} />
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={2.99} factor={1} speed={1}>
+            <Carousel />
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={3}
+            factor={1}
+            speed={2}
+            style={{
+              backgroundImage: `url(${Abba})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={3.99} factor={1} speed={1}>
+            <Carousel />
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={4}
+            factor={1}
+            speed={2}
+            style={{
+              backgroundImage: `url(${Jackson})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={4.99} factor={1} speed={1}>
+            <Carousel />
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={5}
+            factor={1}
+            speed={2}
+            style={{
+              backgroundImage: `url(${Nirvana})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={5.99} factor={1} speed={1}>
+            <Carousel />
           </ParallaxLayer>
         </Parallax>
       </div>
--- client/src/views/Home.view.tsx
+++ client/src/views/Home.view.tsx
@@ -1,70 +1,181 @@
 import React, { useRef } from "react";
 import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
 import { ReactNebula } from "@flodlc/nebula";
-import Carousel from "./components/General/Carousel.component";
 import Header from "./components/General/Header";
-
+import Elvis from "../assets/img/elvis.jpg";
+import Beatles from "../assets/img/beatles.jpg";
+import Carousel from "./components/General/Carousel.component";
+import Jackson from "../assets/img/jackson.jpg";
+import Abba from "../assets/img/abba.jpg";
+import Nirvana from "../assets/img/nirvana.jpg";
 const Home = () => {
   const parallax = useRef<IParallax>(null!);
 
   return (
     <>
       <Header />
-      <Parallax
-        ref={parallax}
-        pages={2}
-        style={{
-          backgroundColor: "#805E73",
-          backgroundSize: "cover",
-        }}
-      >
-        <ParallaxLayer offset={0} speed={1} factor={1}>
-          <div className="w-full h-full object-cover object-left scale-x-[1] mx-auto">
-            <ReactNebula
-              config={{
-                starsCount: 840,
-                starsColor: "#FFFFFF",
-                starsRotationSpeed: 0,
-                cometFrequence: 80,
-                nebulasIntensity: 0,
-                bgColor: "rgb(8,8,8)",
-                sunScale: 0,
-                planetsScale: 0,
-                solarSystemOrbite: 0,
-                solarSystemSpeedOrbit: 0,
-              }}
-            />
-          </div>
-          <div className="w-full h-screen absolute top-0 left-0">
-            <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
-              <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
-                Balul Bobocilor
-              </h1>
+      <div>
+        <Parallax ref={parallax} pages={8}>
+          <ParallaxLayer
+            offset={0}
+            factor={8}
+            speed={0}
+            style={{
+              backgroundImage: `url(${Elvis})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-full h-full object-cover object-left scale-x-[1] mx-auto">
+              <ReactNebula
+                config={{
+                  starsCount: 1000,
+                  starsColor: "#FFFFFF",
+                  starsRotationSpeed: 5,
+                  cometFrequence: 100,
+                  nebulasIntensity: 0,
+                  bgColor: "rgb(8,8,8)",
+                  sunScale: 0,
+                  planetsScale: 0,
+                  solarSystemOrbite: 70,
+                  solarSystemSpeedOrbit: 100,
+                }}
+              />
             </div>
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={1}>
-          <div className="w-full h-full object-cover object-left scale-x-[1] mx-auto">
-            <ReactNebula
-              config={{
-                starsCount: 840,
-                starsColor: "#FFFFFF",
-                starsRotationSpeed: 0,
-                cometFrequence: 80,
-                nebulasIntensity: 0,
-                bgColor: "rgb(8,8,8)",
-                sunScale: 0,
-                planetsScale: 0,
-                solarSystemOrbite: 0,
-                solarSystemSpeedOrbit: 0,
-              }}
-            />
-          </div>
-          <div className="w-full h-screen absolute top-0 left-0">
+            <div className="w-full h-screen absolute top-0 left-0">
+              <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                  Balul Bobocilor
+                </h1>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={1}
+            factor={0.25}
+            speed={1}
+            style={{
+              backgroundImage: `url(${Elvis})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={1.2}
+            speed={2}
+            style={{ opacity: 0.6 }}
+            factor={0.5}
+          >
+            <div className="flex justify-end">
+              <img
+                alt="cloud"
+                src="https://freepngimg.com/thumb/cloud/7-cloud-png-image.png"
+                style={{ width: "20%", marginRight: "5%" }}
+              />
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1.99} factor={1} speed={1}>
             <Carousel />
-          </div>
-        </ParallaxLayer>
-      </Parallax>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={2}
+            factor={1}
+            speed={2}
+            style={{
+              backgroundImage: `url(${Beatles})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={2.99} factor={1} speed={1}>
+            <Carousel />
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={3}
+            factor={1}
+            speed={2}
+            style={{
+              backgroundImage: `url(${Abba})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={3.99} factor={1} speed={1}>
+            <Carousel />
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={4}
+            factor={1}
+            speed={2}
+            style={{
+              backgroundImage: `url(${Jackson})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={4.99} factor={1} speed={1}>
+            <Carousel />
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={5}
+            factor={1}
+            speed={2}
+            style={{
+              backgroundImage: `url(${Nirvana})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-full h-full bg-black/50">
+              <div className="w-full h-screen absolute top-0 left-0">
+                <div className=" max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
+                  <h1 className="sm:text-5xl text-4xl text-gray-300 font-bold">
+                    Componenta
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={5.99} factor={1} speed={1}>
+            <Carousel />
+          </ParallaxLayer>
+        </Parallax>
+      </div>
     </>
   );
 };
