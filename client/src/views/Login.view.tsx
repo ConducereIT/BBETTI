5dcec492535605fb60e85055879a854d96f85ee1
AMIhneaa <asmihnea5@gmail.com>
2023-10-23 20:08:10 +0300

    Final Server

--- client/src/views/Login.view.tsx
+++ client/src/views/Login.view.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { User } from "../sdk/user.sdk";
 
 const Login = () => {
-  return (
-    <>
-      <h1>Login</h1>
-    </>
-  );
+  const [element, setElement] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchElement = async () => {
+      const el = await User.helloWorld();
+      setElement(el);
+    };
+
+    fetchElement();
+  }, []);
+
+  return <div>{element}</div>;
 };
 
 export default Login;
