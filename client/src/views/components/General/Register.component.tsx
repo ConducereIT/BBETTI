65198069a8152cc71d7a4383d586ba1582eed5d6
cristim67 <miloiuc4@gmail.com>
2023-11-09 21:35:00 +0200

    s

--- client/src/views/components/General/Register.component.tsx
+++ client/src/views/components/General/Register.component.tsx
@@ -21,8 +21,8 @@ const RegisterComp = () => {
   const handleSummit = async (event: any) => {
     event.preventDefault();
 
-    const regex = /[^@]+@upb\.ro/;
-    if (!regex.test(user.email)) {
+    const emailRegex = /.*@.*upb\.ro/;
+    if (!emailRegex.test(user.email)) {
       setError("Foloseste un mail valabil de la UPB ");
       return;
     } else {
