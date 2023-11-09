468f8a1dbf90057dec9147591fb5a4980eb1f2a0
cristim67 <miloiuc4@gmail.com>
2023-11-09 19:40:45 +0200

    update

--- client/src/views/components/General/UserTable.component.tsx
+++ client/src/views/components/General/UserTable.component.tsx
@@ -1,47 +1,55 @@
-import React from 'react';
+import React from "react";
 import Header from "./Header";
 
-const UserTableAdmin = ( {data}:any ) => {
-  return (
-    data.length!=0 ?(
-      <>
-      <Header/>
-    <div className="mt-20">
-      <h2>User Table</h2>
-      <table>
-        <thead>
-        <tr >
-          <th>User ID</th>
-          <th>Email</th>
-          <th>Vote Fata</th>
-          <th>Vote Baiat</th>
-          <th>Vote Fata Name</th>
-          <th>Vote Baiat Name</th>
-          <th>Admin</th>
-          <th>Verified</th>
-          <th>Cod confirmare email</th>
-        </tr>
-        </thead>
-        <tbody>
-        {data.map((user:any, index:any) => (
-          <tr key={index} className="text-white">
-            <td>{index}</td>
-            <td>{user.email}</td>
-            <td>{user.voteFata ? 'Da' : 'Nu'}</td>
-            <td>{user.voteBaiat ? 'Da' : 'Nu'}</td>
-            <td>{user.voteFataName}</td>
-            <td>{user.voteBaiatName}</td>
-            <td>{user.admin || 'N/A'}</td>
-            <td>{user.verified ? 'Da' : 'Nu'}</td>
-            <td>{user.tokenConfirmEmail}</td>
-          </tr>
-        ))
-        }
-        </tbody>
-      </table>
-    </div>
-      </>):(<div>Salut</div>)
-
+const UserTableAdmin = ({ data,adminType }:any) => {
+  console.log("AdminType în UserTableAdmin:", adminType);
+  return data.length != 0 ? (
+    <>
+      <Header />
+      <div className="mt-20">
+        <h2>User Table</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>User ID</th>
+              <th>Email</th>
+              <th>Vote Fata</th>
+              <th>Vote Baiat</th>
+              {adminType === "superadmin" ? (
+                <>
+                  <th>Vote Fata Name</th>
+                  <th>Vote Baiat Name</th>
+                </>
+              ) : null}
+              <th>Admin</th>
+              <th>Verified</th>
+              <th>Cod confirmare email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user: any, index: any) => (
+              <tr key={index} className="text-white">
+                <td>{index}</td>
+                <td>{user.email}</td>
+                <td>{user.voteFata ? "Da" : "Nu"}</td>
+                <td>{user.voteBaiat ? "Da" : "Nu"}</td>
+                {adminType === "superadmin" ? (
+                  <>
+                    <td>{user.voteFataName}</td>
+                    <td>{user.voteBaiatName}</td>
+                  </>
+                ) : null}
+                <td>{user.admin || "N/A"}</td>
+                <td>{user.verified ? "Da" : "Nu"}</td>
+                <td>{user.tokenConfirmEmail}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  ) : (
+    <div>Salut</div>
   );
 };
 
