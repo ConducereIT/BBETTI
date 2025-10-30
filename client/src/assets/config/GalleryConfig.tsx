52e9b955936029a2bf3d5f3c7cf0aa3fdf9fb122
CristiSx <cristiansalafenco@gmail.com>
2025-10-30 17:28:07 +0200

    new photos, added the 2024 gallery

--- client/src/assets/config/GalleryConfig.tsx
+++ client/src/assets/config/GalleryConfig.tsx
@@ -1,53 +1,42 @@
-import PozaDesk1 from "../img/Carousel/bbetti1.jpg";
-import PozaDesk2 from "../img/Carousel/bbetti2.jpg";
-import PozaDesk3 from "../img/Carousel/bbetti3.jpg";
-import PozaDesk4 from "../img/Carousel/bbetti4.jpg";
-import PozaDesk5 from "../img/Carousel/bbetti5.jpg";
-import PozaDesk6 from "../img/Carousel/bbetti6.jpg";
-import PozaDesk7 from "../img/Carousel/bbetti7.jpg";
-import PozaMobil11 from "../img/Carousel/bbetti11.jpg";
-import PozaMobil12 from "../img/Carousel/bbetti12.jpg";
-import PozaMobil13 from "../img/Carousel/bbetti13.jpg";
-import PozaMobil14 from "../img/Carousel/bbetti14.jpg";
-import PozaMobil15 from "../img/Carousel/bbetti15.jpg";
-import PozaMobil16 from "../img/Carousel/bbetti16.jpg";
-import PozaMobil17 from "../img/Carousel/bbetti17.jpg";
+import bbetti1 from "../img/Carousel/bbetti1.webp";
+import bbetti2 from "../img/Carousel/bbetti2.webp";
+import bbetti3 from "../img/Carousel/bbetti3.webp";
+
+import bbetti5 from "../img/Carousel/bbetti5.webp";
+
+import bbetti8 from "../img/Carousel/bbetti8.webp";
+
+
 
 export const GalleryImages = [
   {
-    imageDesktop: PozaDesk1,
-    imageMobile: PozaMobil11,
+    imageDesktop: bbetti1,
+    imageMobile: bbetti1,
     alt: "ceva",
   },
   {
-    imageDesktop: PozaDesk2,
-    imageMobile: PozaMobil12,
+    imageDesktop: bbetti2,
+    imageMobile: bbetti2,
     alt: "ceva",
   },
   {
-    imageDesktop: PozaDesk3,
-    imageMobile: PozaMobil13,
-    alt: "ceva",
-  },
-  {
-    imageDesktop: PozaDesk4,
-    imageMobile: PozaMobil14,
+    imageDesktop: bbetti3,
+    imageMobile: bbetti3,
     alt: "ceva",
   },
 
   {
-    imageDesktop: PozaDesk5,
-    imageMobile: PozaMobil15,
+    imageDesktop: bbetti5,
+    imageMobile: bbetti5,
     alt: "ceva",
   },
+
+
   {
-    imageDesktop: PozaDesk6,
-    imageMobile: PozaMobil16,
+    imageDesktop: bbetti8,
+    imageMobile: bbetti8,
     alt: "ceva",
   },
-  {
-    imageDesktop: PozaDesk7,
-    imageMobile: PozaMobil17,
-    alt: "ceva",
-  },
+
+
 ];
